dba4aa62f92988b537c48a1ad17e5ebc8e86cfa6
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-03-01 18:41:46 +0300

    Created User related classes

--- app/src/main/java/com/graduationproject/grad_project/model/Administrator.kt
+++ app/src/main/java/com/graduationproject/grad_project/model/Administrator.kt
@@ -1,5 +1,16 @@
 package com.graduationproject.grad_project.model
 
-class Administrator {
+class Administrator(
+    override var fullName: String,
+    override var email: String,
+    override var password: String,
+    override var phoneNumber: String,
+    override var siteName: String,
+    override var location: Location,
+    private var adminKey : String
+) : User() {
+    init {
+        authorizationToAdminInterface = true
+    }
 
 }
