4c88a9839357c6848af550111e552611df15a55b
Batuhan-G <batuhangoren99@hotmail.com>
2022-03-09 13:54:18 +0300

    Home page bottom navigation design
    bottom navigation added
    added navigation drawer
    language string updated

--- app/src/main/java/com/graduationproject/grad_project/view/Admin/HomePageAdminActivity.kt
+++ app/src/main/java/com/graduationproject/grad_project/view/Admin/HomePageAdminActivity.kt
@@ -2,11 +2,17 @@ package com.graduationproject.grad_project.view.Admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.graduationproject.grad_project.R
 
 class HomePageAdminActivity : AppCompatActivity() {
+    private lateinit var bottom_nav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page_admin)
+        val navController= findNavController(R.id.hostFragment)
+        bottom_nav.setupWithNavController(navController)
     }
 }
