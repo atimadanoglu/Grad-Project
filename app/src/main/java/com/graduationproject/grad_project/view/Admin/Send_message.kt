f9e8c0f2c8893b060c280f5781a31e6905e0d702
Batuhan-G <batuhangoren99@hotmail.com>
2022-03-13 22:56:15 +0300

    REDESIGNED announement, resident list -ADMIN-

---
+++ app/src/main/java/com/graduationproject/grad_project/view/Admin/Send_message.kt
@@ -0,0 +1,12 @@
+package com.graduationproject.grad_project.view.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.graduationproject.grad_project.R
+
+class Send_message : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_message)
+    }
+}
