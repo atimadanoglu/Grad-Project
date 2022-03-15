2e94077c07a01a97aca5d474e26cf9027c0f2bef
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-03-15 14:35:11 +0300

    Implemented filtering residents functionality
    *Made some improvements on methods. *Shortened methods

--- app/src/main/java/com/graduationproject/grad_project/adapter/ResidentsListRecyclerViewAdapter.kt
+++ app/src/main/java/com/graduationproject/grad_project/adapter/ResidentsListRecyclerViewAdapter.kt
@@ -8,11 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.graduationproject.grad_project.R
 import com.graduationproject.grad_project.model.SiteResident
 
-class ResidentsListRecyclerViewAdapter(private val residents : ArrayList<SiteResident>) :
+class ResidentsListRecyclerViewAdapter(private var residents : ArrayList<SiteResident>) :
     RecyclerView.Adapter<ResidentsListRecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-        private val accountName : TextView = itemView.findViewById(R.id.account_name_list_item)
+        private val accountName : TextView = itemView.findViewById(R.id.account_name_text_list_item)
         private val block : TextView = itemView.findViewById(R.id.block_no_text_list_item)
         private val flatNo : TextView = itemView.findViewById(R.id.flat_no_text_list_item)
         private val debtText : TextView = itemView.findViewById(R.id.debt_text_list_item)
@@ -38,4 +38,8 @@ class ResidentsListRecyclerViewAdapter(private val residents : ArrayList<SiteRes
         return residents.count()
     }
 
+    fun filterList(list: ArrayList<SiteResident>) {
+        residents = list
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/graduationproject/grad_project/view/Admin/ResidentsListForAdminFragment.kt
+++ app/src/main/java/com/graduationproject/grad_project/view/Admin/ResidentsListForAdminFragment.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.*
 import com.graduationproject.grad_project.R
 import com.graduationproject.grad_project.adapter.ResidentsListRecyclerViewAdapter
 import com.graduationproject.grad_project.databinding.FragmentResidentsListForAdminBinding
+import com.graduationproject.grad_project.model.Site
 import com.graduationproject.grad_project.model.SiteResident
 
 class ResidentsListForAdminFragment : Fragment() {
@@ -22,6 +26,8 @@ class ResidentsListForAdminFragment : Fragment() {
     private var residentsListRecyclerViewAdapter : ResidentsListRecyclerViewAdapter? = null
     private lateinit var db : FirebaseFirestore
     private lateinit var auth : FirebaseAuth
+    private val siteResidents = ArrayList<SiteResident>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,43 +40,82 @@ class ResidentsListForAdminFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentResidentsListForAdminBinding.inflate(inflater, container, false)
         retrieveAndShowResidents()
+
+        binding.searchBar.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter(newText)
+                return false
+            }
+        })
         return binding.root
     }
 
+    // It gives the filtered result
+    private fun filter(newText: String?) {
+        val filteredList = ArrayList<SiteResident>()
+        for (resident in siteResidents) {
+            if (resident.fullName.contains(newText.toString(), true)
+                || resident.flatNo.toString().contains(newText.toString())
+                || resident.blockNo.contains(newText.toString(), true)
+            ) {
+                filteredList.add(resident)
+            }
+        }
+        residentsListRecyclerViewAdapter?.filterList(filteredList)
+    }
+
+    private fun getResidents(adminInfo: DocumentSnapshot) : Task<QuerySnapshot> {
+        return db.collection("residents")
+            .whereEqualTo("city", adminInfo["city"])
+            .whereEqualTo("district", adminInfo["district"])
+            .whereEqualTo("siteName", adminInfo["siteName"])
+            .orderBy("flatNo", Query.Direction.ASCENDING)
+            .get()
+    }
+
+    private fun getAdminInfo(email: String): Task<DocumentSnapshot> {
+        return db.collection("administrators")
+            .document(email)
+            .get()
+    }
+
     private fun retrieveAndShowResidents() {
-        val siteResidents = ArrayList<SiteResident>()
         auth.currentUser?.email?.let { email ->
-            db.collection("administrators")
-                .document(email)
-                .get()
-                .addOnSuccessListener {
-                    val adminInfo = it
-                    Log.d("admin", "Admin info retrieved")
-                    db.collection("residents")
-                        .whereEqualTo("city", adminInfo["city"])
-                        .whereEqualTo("district", adminInfo["district"])
-                        .whereEqualTo("siteName", adminInfo["siteName"])
-                        .get()
-                        .addOnSuccessListener { residents ->
-                            Log.d("AddAnnouncementFragment", "get() successfully worked")
-                                for (resident in residents) {
-                                    siteResidents.add(SiteResident(
-                                        fullName = resident.get("fullName") as String,
-                                        email = resident.get("email") as String,
-                                        password = resident.get("password") as String,
-                                        phoneNumber = resident.get("phoneNumber") as String,
-                                        blockNo = resident.get("blockNo") as String,
-                                        flatNo = resident.get("flatNo").toString().toInt(),
-                                        debt = resident.get("debt") as Double
-                                    ))
-                                    binding.recyclerview.layoutManager = LinearLayoutManager(this.context)
-                                    residentsListRecyclerViewAdapter = ResidentsListRecyclerViewAdapter(siteResidents)
-                                    binding.recyclerview.adapter = residentsListRecyclerViewAdapter
-                                }
+            getAdminInfo(email).addOnSuccessListener {
+                val adminInfo = it
+                Log.d("admin", "Admin info retrieved")
+                getResidents(adminInfo)
+                    .addOnSuccessListener { residents ->
+                        Log.d("AddAnnouncementFragment", "get() successfully worked")
+                        for (resident in residents) {
+                            addResident(siteResidents, resident)
+                            adaptResidentsListRecyclerView(siteResidents)
                         }
-                }
+                    }
+            }
         }
     }
 
+    private fun addResident(siteResidents: ArrayList<SiteResident>, resident: QueryDocumentSnapshot) {
+        siteResidents.add(SiteResident(
+            fullName = resident.get("fullName") as String,
+            email = resident.get("email") as String,
+            password = resident.get("password") as String,
+            phoneNumber = resident.get("phoneNumber") as String,
+            blockNo = resident.get("blockNo") as String,
+            flatNo = resident.get("flatNo").toString().toInt(),
+            debt = resident.get("debt") as Double
+        ))
+    }
+
+    private fun adaptResidentsListRecyclerView(siteResidents: ArrayList<SiteResident>) {
+        binding.recyclerview.layoutManager = LinearLayoutManager(this.context)
+        residentsListRecyclerViewAdapter = ResidentsListRecyclerViewAdapter(siteResidents)
+        binding.recyclerview.adapter = residentsListRecyclerViewAdapter
+    }
 
 }
