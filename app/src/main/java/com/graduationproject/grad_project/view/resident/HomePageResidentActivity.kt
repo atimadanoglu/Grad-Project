534ae66ec8817188664e65f6c16eae89b7082d23
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-05 16:30:56 +0300

    Implemented showing resident header feature

--- app/src/main/java/com/graduationproject/grad_project/view/resident/HomePageResidentActivity.kt
+++ app/src/main/java/com/graduationproject/grad_project/view/resident/HomePageResidentActivity.kt
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.graduationproject.grad_project.R
 import com.graduationproject.grad_project.databinding.ActivityHomePageResidentBinding
+import com.graduationproject.grad_project.databinding.DrawerHeaderBinding
 
 class HomePageResidentActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomePageResidentBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageResidentBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        auth = FirebaseAuth.getInstance()
         val navHostFragment = binding.mainFragmentContainerViewForResident.getFragment() as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
         binding.navigationView.setupWithNavController(navController)
+        auth.currentUser?.let { setHeader(it) }
+    }
+
+    private fun setHeader(currentUser: FirebaseUser) {
+        val header = binding.navigationView.getHeaderView(0)
+        val drawerHeaderBinding = DrawerHeaderBinding.bind(header)
+
+        drawerHeaderBinding.headerAccountName.text = currentUser.displayName
+        drawerHeaderBinding.headerAccountType.setText(R.string.sakin)
+        drawerHeaderBinding.headerEmailAddress.text = currentUser.email
     }
 
 
