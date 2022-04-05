e1596d0276db7c5b63e1e918cebbcfc5e20fe5d7
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-05 17:57:09 +0300

    Added navigation arguments

--- app/src/main/java/com/graduationproject/grad_project/SignUpMainFragment.kt
+++ app/src/main/java/com/graduationproject/grad_project/SignUpMainFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 class SignUpMainFragment : Fragment() {
-    
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
