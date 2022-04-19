461c91b48b29997905e673384e0f0ff2c7071c5b
Batuhan-G <batuhangoren99@hotmail.com>
2022-04-19 19:27:15 +0300

    Designed Site expenditures Resident
    Also
    Alert Dialog Design have been made:
    success alert
    error alert
    confirm alert

---
+++ app/src/main/java/com/graduationproject/grad_project/view/SuccessfulDialogFragment.kt
@@ -0,0 +1,20 @@
+package com.graduationproject.grad_project.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.graduationproject.grad_project.R
+
+class SuccessfulDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_successful_dialog, container, false)
+    }
+
+}
