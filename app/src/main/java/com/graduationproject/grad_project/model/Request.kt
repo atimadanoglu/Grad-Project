831ec78465455ad419b8aafe4957e8a4b8b72ef4
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-24 19:12:52 +0300

    Updated requestsFragment's recyclerView, fragment and viewModel

--- app/src/main/java/com/graduationproject/grad_project/model/Request.kt
+++ app/src/main/java/com/graduationproject/grad_project/model/Request.kt
@@ -3,10 +3,10 @@ package com.graduationproject.grad_project.model
 import com.google.firebase.Timestamp
 
 data class Request(
-    val id: String,
-    val title: String,
-    val content: String,
-    val type: String,
-    val sentBy: String,
-    val date: Timestamp
+    val id: String = "",
+    val title: String = "",
+    val content: String = "",
+    val type: String = "",
+    val sentBy: String = "",
+    val date: Timestamp? = null
 )
