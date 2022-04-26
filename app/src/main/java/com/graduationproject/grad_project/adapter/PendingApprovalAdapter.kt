c4efd68b4cd6538e743351e08914e49571214e9a
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-26 16:52:26 +0300

    Updated string resource file

--- app/src/main/java/com/graduationproject/grad_project/adapter/PendingApprovalAdapter.kt
+++ app/src/main/java/com/graduationproject/grad_project/adapter/PendingApprovalAdapter.kt
@@ -1,6 +1,7 @@
 package com.graduationproject.grad_project.adapter
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -39,21 +40,23 @@ class PendingApprovalAdapter(private val context: Context):
             showAlertMessageForAcceptButton(item)
         }
         holder.binding.rejectButton.setOnClickListener {
-            showAlertMessageForRejectButton()
+            showAlertMessageForRejectButton(item)
         }
     }
     private fun showAlertMessageForAcceptButton(resident: SiteResident) {
+        val text = context.resources.getString(R.string.siteSakinOnayı, resident.fullName)
         MaterialAlertDialogBuilder(context)
-            .setMessage(R.string.eminMisiniz)
+            .setMessage(text)
             .setPositiveButton(R.string.evet) { _, _ ->
                 UserOperations.acceptResident(resident)
             }.setNegativeButton(R.string.hayır) { _, _ -> }
             .create().show()
     }
 
-    private fun showAlertMessageForRejectButton() {
+    private fun showAlertMessageForRejectButton(resident: SiteResident) {
+        val text = context.resources.getString(R.string.siteSakinReddi, resident.fullName)
         MaterialAlertDialogBuilder(context)
-            .setMessage(R.string.eminMisiniz)
+            .setMessage(text)
             .setPositiveButton(R.string.evet) { _, _ -> }
             .setNegativeButton(R.string.hayır) { _, _ -> }
             .create().show()
