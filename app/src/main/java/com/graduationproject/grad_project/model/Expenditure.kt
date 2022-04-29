7004eedff730b7066f6bfdcd3a057d47cc9e8f94
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-04-29 08:01:45 +0300

    Implemented expenditures list for resident

--- app/src/main/java/com/graduationproject/grad_project/model/Expenditure.kt
+++ app/src/main/java/com/graduationproject/grad_project/model/Expenditure.kt
@@ -3,10 +3,10 @@ package com.graduationproject.grad_project.model
 import com.google.firebase.Timestamp
 
 data class Expenditure(
-    val id: String,
-    val title: String,
-    val content: String,
-    val amount: Int,
-    val documentUri: String,
-    val date: Timestamp
+    val id: String = "",
+    val title: String = "",
+    val content: String = "",
+    val amount: Long = 0,
+    val documentUri: String = "",
+    val date: Timestamp? = null
 )
