ac0acac9ce2660a7cd22cae0f41aafe4ae1fcb60
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-07 19:43:33 +0300

    Implemented date picker functionality

---
+++ app/src/main/java/com/graduationproject/grad_project/view/Admin/dialogs/DatePickerDialogFragment.kt
@@ -0,0 +1,45 @@
+package com.graduationproject.grad_project.view.admin.dialogs
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Message
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import com.graduationproject.grad_project.R
+import com.graduationproject.grad_project.viewmodel.AddVotingViewModel
+import java.util.*
+
+
+class DatePickerDialogFragment(private val viewModel: AddVotingViewModel)
+    : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    private val calendar: Calendar by lazy {
+        Calendar.getInstance()
+    }
+    // two weeks in milliseconds
+    private val twoWeeks = 604800000L * 2
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val newDialog = DatePickerDialog(it, this, year, month, day)
+            newDialog.create()
+            newDialog.datePicker.minDate = calendar.timeInMillis
+            newDialog.datePicker.maxDate = calendar.timeInMillis + twoWeeks
+            newDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).setText(R.string.tamam)
+            newDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setText(R.string.iptal)
+            newDialog
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        viewModel.saveChosenDate("$day-${month + 1}-$year")
+        calendar.set(year, month, day)
+        viewModel.setDateLongValue(calendar.timeInMillis)
+    }
+
+}
