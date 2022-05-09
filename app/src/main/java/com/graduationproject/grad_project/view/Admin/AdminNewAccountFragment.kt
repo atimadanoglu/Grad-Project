422eff8c1274e887bb49330ad2d2fc1e8c33a02c
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-10 02:39:04 +0300

    Updated ResidentNewAccountFragment and its viewModel

--- app/src/main/java/com/graduationproject/grad_project/view/Admin/AdminNewAccountFragment.kt
+++ app/src/main/java/com/graduationproject/grad_project/view/Admin/AdminNewAccountFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.graduationproject.grad_project.R
 import com.graduationproject.grad_project.components.SnackBars
 import com.graduationproject.grad_project.databinding.FragmentAdminNewAccountBinding
 import com.graduationproject.grad_project.viewmodel.AdminNewAccountViewModel
@@ -24,10 +26,60 @@ class AdminNewAccountFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentAdminNewAccountBinding.inflate(inflater, container, false)
         val view = binding.root
-        binding.backToSignUpMainFragmentButton.setOnClickListener { goBackToSignUpMainFragment() }
-        binding.goToAdminSiteInfoButton.setOnClickListener { goToSiteInformationButtonClicked() }
-        binding.goToLoginPageButton.setOnClickListener { goBackToLoginFragment() }
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        emailAddressTextFocusChangeListener()
+        phoneNumberTextFocusChangeListener()
+        passwordTextChangeListener()
+        setOnClickListeners()
         return view
+    }
+
+    private fun setOnClickListeners() {
+        binding.backToSignUpMainFragmentButton.setOnClickListener { goBackToSignUpMainFragment() }
+        binding.goToAdminSiteInfoButton.setOnClickListener {
+            if (viewModel.allAreValid()) {
+                goToSiteInformationButtonClicked()
+            }
+        }
+        binding.goToLoginPageButton.setOnClickListener { goBackToLoginFragment() }
+    }
+
+    private fun emailAddressTextFocusChangeListener() {
+        binding.email.setOnFocusChangeListener { _, b ->
+            if (!b) {
+                if (!viewModel.isValidEmail()) {
+                    val errorMessage = resources.getString(R.string.geçersizEmailAdresi)
+                    binding.emailLayout.error = errorMessage
+                } else {
+                    binding.emailLayout.error = null
+                }
+            }
+        }
+    }
+
+    private fun phoneNumberTextFocusChangeListener() {
+        binding.phoneNumber.setOnFocusChangeListener { _, b ->
+            if (!b) {
+                if (!viewModel.isValidPhoneNumber()) {
+                    val errorMessage = resources.getString(R.string.geçersizTelefonNo)
+                    binding.phoneNumberLayout.error = errorMessage
+                } else {
+                    binding.phoneNumberLayout.error = null
+                }
+            }
+        }
+    }
+
+    private fun passwordTextChangeListener() {
+        binding.password.addTextChangedListener {
+            if (!viewModel.isValidPassword()) {
+                val errorMessage = resources.getString(R.string.şifreBilgi)
+                binding.passwordLayout.error = errorMessage
+            } else {
+                binding.passwordLayout.error = null
+            }
+        }
     }
 
     private fun goBackToLoginFragment() {
@@ -39,18 +91,17 @@ class AdminNewAccountFragment : Fragment() {
     private fun goToSiteInformationFragment() {
         val action = AdminNewAccountFragmentDirections
             .actionAdminNewAccountFragmentToAdminSiteInformationFragment(
-                viewModel.fullName,
-                viewModel.phoneNumber,
-                viewModel.email,
-                viewModel.password
+                viewModel.fullName.value!!,
+                viewModel.phoneNumber.value!!,
+                viewModel.email.value!!,
+                viewModel.password.value!!
             )
         findNavController().navigate(action)
     }
 
     private fun goToSiteInformationButtonClicked() {
-        updateViewModelData()
-        if (!isBlank()) {
-            if (isActivationKeyCorrect()) {
+        if (!viewModel.isEmpty()) {
+            if (viewModel.isActivationKeyCorrect()) {
                 goToSiteInformationFragment()
                 return
             } else {
@@ -60,22 +111,6 @@ class AdminNewAccountFragment : Fragment() {
             SnackBars.showEmptySpacesSnackBar(view)
         }
     }
-
-    private fun updateViewModelData() {
-        viewModel.setFullName(binding.fullNameText.text.toString())
-        viewModel.setEmail(binding.TextEmailAddress.text.toString())
-        viewModel.setPhoneNumber(binding.phoneNumberText.text.toString())
-        viewModel.setPassword(binding.TextPassword.text.toString())
-        viewModel.setActivationKey(binding.ActivationKey.text.toString())
-    }
-
-    private fun isBlank(): Boolean {
-        return viewModel.email.isBlank() || viewModel.fullName.isBlank()
-                || viewModel.activationKey.isBlank() || viewModel.phoneNumber.isBlank()
-                || viewModel.password.isBlank()
-    }
-
-    private fun isActivationKeyCorrect(): Boolean = viewModel.activationKey == "qwerty"
 
     private fun goBackToSignUpMainFragment() {
         val action = AdminNewAccountFragmentDirections.actionAdminNewAccountFragmentToSignUpMainFragment()
