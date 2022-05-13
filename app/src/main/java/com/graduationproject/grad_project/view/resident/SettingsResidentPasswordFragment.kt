41e06734b8bd87dd786848d6479e2d9eb8037bdc
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-13 23:05:16 +0300

    Implemented settingsResidentPassword fragment

---
+++ app/src/main/java/com/graduationproject/grad_project/view/resident/SettingsResidentPasswordFragment.kt
@@ -0,0 +1,109 @@
+package com.graduationproject.grad_project.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.graduationproject.grad_project.R
+import com.graduationproject.grad_project.databinding.FragmentSettingsResidentPasswordBinding
+import com.graduationproject.grad_project.viewmodel.SettingsResidentPasswordViewModel
+
+class SettingsResidentPasswordFragment : Fragment() {
+
+    private var _binding: FragmentSettingsResidentPasswordBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SettingsResidentPasswordViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSettingsResidentPasswordBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.updateButton.setOnClickListener { passwordUpdateButtonClicked() }
+        binding.oldpasswordEditText.addTextChangedListener {
+            if (it.isNullOrEmpty()) {
+                binding.oldPasswordLayout.error = "Boş olamaz!"
+            } else {
+                binding.oldPasswordLayout.error = null
+            }
+        }
+        binding.newPassword.addTextChangedListener {
+            if (it.isNullOrEmpty()) {
+                binding.newPasswordLayout.error = "Boş olamaz!"
+            } else {
+                binding.newPasswordLayout.error = null
+            }
+        }
+        return binding.root
+    }
+
+    private fun passwordUpdateButtonClicked() {
+        val previousPassword = binding.oldpasswordEditText.text.toString()
+        val newPassword = binding.newPassword.text.toString()
+        val repeatedPassword = binding.repeatPasswordEditText.text.toString()
+        isValid(previousPassword, newPassword, repeatedPassword)
+        if (!viewModel.areEmptyOrNull()) {
+            viewModel.reAuthenticateUser()
+        }
+        viewModel.isAuthenticated.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    viewModel.updatePassword()
+                }
+            }
+        }
+        viewModel.isChanged.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    Snackbar.make(
+                        requireView(),
+                        R.string.şifreDeğiştirmeBaşarılı,
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    goBackToSettings()
+                } else {
+                    Snackbar.make(
+                        requireView(),
+                        R.string.şifreDeğiştirmeBaşarısız,
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun goBackToSettings() {
+        val action = SettingsResidentPasswordFragmentDirections.actionSettingsResidentPasswordFragmentToSettingsResidentFragment()
+        requireView().findNavController().navigate(action)
+    }
+
+    private fun isValid(previousPassword: String, newPassword: String, repeatedPassword: String) {
+        if (newPassword.length in 1 until 8) {
+            binding.newPasswordLayout.error = "Şifre en az 8 karakter olmalı!"
+        } else {
+            binding.newPasswordLayout.error = null
+        }
+        if (newPassword.isEmpty()) {
+            binding.newPasswordLayout.error = "Boş olamaz!"
+        } else {
+            binding.newPasswordLayout.error = null
+        }
+        if (repeatedPassword.isEmpty()) {
+            binding.repeatPasswordLayout.error = "Boş olamaz!"
+        } else {
+            binding.repeatPasswordLayout.error = null
+        }
+        if (previousPassword.isEmpty()) {
+            binding.oldPasswordLayout.error = "Boş olamaz!"
+        } else {
+            binding.oldPasswordLayout.error = null
+        }
+    }
+}
---
+++ app/src/main/java/com/graduationproject/grad_project/viewmodel/SettingsResidentPasswordViewModel.kt
@@ -0,0 +1,86 @@
+package com.graduationproject.grad_project.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SettingsResidentPasswordViewModel: ViewModel() {
+
+    companion object {
+        const val TAG = "SettingsPasswordViewModel"
+    }
+
+    private val auth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    val previousPassword = MutableLiveData("")
+    val newPassword = MutableLiveData("")
+    val repeatedNewPassword = MutableLiveData("")
+
+    private val _isChanged = MutableLiveData<Boolean?>()
+    val isChanged: LiveData<Boolean?> get() = _isChanged
+
+    private val _isAuthenticated = MutableLiveData<Boolean?>()
+    val isAuthenticated: LiveData<Boolean?> get() = _isAuthenticated
+
+    fun updatePassword() = viewModelScope.launch(Dispatchers.IO) {
+        try {
+            val currentUser = auth.currentUser
+            currentUser?.let {
+                if (arePasswordSame()) {
+                    newPassword.value?.let { password ->
+                        it.updatePassword(password).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Log.d(TAG, "updatePassword -> SUCCESSFUL!")
+                                _isChanged.postValue(true)
+                            } else {
+                                Log.d(TAG, "updatePassword -> UNSUCCESSFUL!")
+                                _isChanged.postValue(false)
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e: FirebaseException) {
+            Log.e(TAG, "updatePassword --> $e")
+            _isChanged.postValue(null)
+        }
+    }
+
+    private fun arePasswordSame() = newPassword.value == repeatedNewPassword.value
+            && !newPassword.value.isNullOrEmpty() && !repeatedNewPassword.value.isNullOrEmpty()
+
+    fun areEmptyOrNull() = newPassword.value.isNullOrEmpty() || !repeatedNewPassword.value.isNullOrEmpty()
+            && previousPassword.value.isNullOrEmpty()
+
+    fun reAuthenticateUser() = viewModelScope.launch(Dispatchers.IO) {
+        try {
+            val user = auth.currentUser
+            user?.let { currentUser ->
+                previousPassword.value?.let { previousPassword ->
+                    val credential = EmailAuthProvider.getCredential(user.email!!, previousPassword)
+                    currentUser.reauthenticate(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.d(TAG, "reAuthenticateUser -> SUCCESSFUL!")
+                            _isAuthenticated.postValue(true)
+                        } else {
+                            Log.d(TAG, "reAuthenticateUser -> UNSUCCESSFUL!")
+                            _isAuthenticated.postValue(false)
+                        }
+                    }
+                }
+            }
+        } catch (e: FirebaseException) {
+            Log.e(TAG, "reAuthenticateUser -> $e")
+            _isAuthenticated.postValue(null)
+        }
+    }
+}
