fa533d582a065e1f4114b6fb09b23b609d826f9b
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-13 23:06:36 +0300

    Implemented settingsResidentPhone fragment

---
+++ app/src/main/java/com/graduationproject/grad_project/view/resident/SettingsResidentPhoneFragment.kt
@@ -0,0 +1,52 @@
+package com.graduationproject.grad_project.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.graduationproject.grad_project.R
+import com.graduationproject.grad_project.databinding.FragmentSettingsResidentPhoneBinding
+import com.graduationproject.grad_project.viewmodel.SettingsResidentPhoneViewModel
+
+class SettingsResidentPhoneFragment : Fragment() {
+
+    private var _binding: FragmentSettingsResidentPhoneBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SettingsResidentPhoneViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSettingsResidentPhoneBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.updateButton.setOnClickListener {
+            viewModel.updatePhoneNumber()
+        }
+        viewModel.isSuccessful.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    goToSettingsPage()
+                } else {
+                    Snackbar.make(
+                        requireView(),
+                        R.string.telefonNoDeğiştirilemedi,
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        return binding.root
+    }
+
+    private fun goToSettingsPage() {
+        val action = SettingsResidentPhoneFragmentDirections.actionSettingsResidentPhoneFragmentToSettingsResidentFragment()
+        findNavController().navigate(action)
+    }
+}
